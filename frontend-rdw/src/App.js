5333806e9fd51835f7279591a114a18b83b378a2
marijndegen <marijndegen96@hotmail.com>
2022-02-21 13:43:45 +0100

    Update App, put header in seperate component.

--- frontend-rdw/src/App.js
+++ frontend-rdw/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
+import NavBar from "./Components/NavBar";
+
 import EditCar from "./Pages/EditCar";
 import ListCars from "./Pages/ListCars";
 import NewCar from "./Pages/NewCar";
@@ -11,20 +13,16 @@ class App extends Component {
   render = () => {
     return (
       <Router>
-        <nav>
-          <Link to="/"> EditCar </Link>
-          <Link to="/list"> ListCars </Link>
-          <Link to="/new"> NewCar </Link>
-          <Link to="/view"> ViewCar </Link>
-        </nav>
+        <NavBar />
         <Routes>
-          <Route path="/" element={<EditCar />} />
+          <Route path="/" element={<h1>Select an action</h1>} />
+          <Route path="/edit" element={<EditCar />} />
           <Route path="/list" element={<ListCars />} />
           <Route path="/new" element={<NewCar />} />
-          <Route path="/view" element={<ViewCar />} />
+          <Route path="/view/:carId" element={<ViewCar />} />
           <Route path="*" element={<h1>Not  found</h1>} />
         </Routes>
-      </Router>
+      </Router >
 
     );
   }
