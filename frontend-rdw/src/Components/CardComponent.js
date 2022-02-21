4574310268e9757fe02c199c7ee66836ecc944a2
marijndegen <marijndegen96@hotmail.com>
2022-02-21 13:09:16 +0100

    Make card completly part of link and update tailwind

--- frontend-rdw/src/Components/CardComponent.js
+++ frontend-rdw/src/Components/CardComponent.js
@@ -1,17 +1,21 @@
 import React, { Component } from "react";
 
+import { Link } from "react-router-dom";
+
 class CardComponent extends Component {
     render = () => {
-        return (<div class="w-full flex p-3 pl-4 items-center hover:bg-gray-300 rounded-lg cursor-pointer">
-            <div>
-                <div class="font-bold text-lg">Name: Tom</div>
-                <div class="text-xs text-gray-500">
-                    <span class="mr-2">No: 007886</span>
-                    <span class="mr-2">gender: man</span>
-                    <span class="mr-2">hobby: skiing</span>
-                </div>
-            </div>
-        </div>);
+        return (
+            <Link to={`/view/${this.props.id}`} >
+                <div className="w-full p-3 pl-4 hover:bg-gray-300 rounded-lg cursor-pointer">
+                    <div className="font-bold text-lg ">Plate: {this.props.plate_number}</div>
+                    <div className="text-xs text-gray-500">
+                        <span className="mr-2">Brand: {this.props.brand}</span>
+                        <span className="mr-2">Model: {this.props.model}</span>
+                        <span className="mr-2">Body type: {this.props.body_type}</span>
+                    </div>
+                </div >
+            </Link >
+        );
     }
 }
 
